2069f6e35d02e1adbd255b3f15ba171fdc9fc5ca
sabotenpillow <saboten.pillow@gmail.com>
2020-05-08 17:01:30 +0000

    [modify] not allow to access signup

--- Scheduler/accounts/views.py
+++ Scheduler/accounts/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.http import HttpResponse
 
 class SignUpView(generic.CreateView):
-    form_class    = UserCreationForm
-    success_url   = reverse_lazy('login')
-    template_name = 'accounts/signup.html'
+    #form_class    = UserCreationForm
+    #success_url   = reverse_lazy('login')
+    #template_name = 'accounts/signup.html'
+    def dispatch(self, *args, **kwargs):
+        return HttpResponse("not allowed")
