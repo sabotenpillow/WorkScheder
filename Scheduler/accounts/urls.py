a968d656836e14be4458222e3db85daf6aabd86d
sabotenpillow <saboten.pillow@gmail.com>
2020-06-21 19:34:29 +0000

    [bugfix & modify] fix bug not redirecting change-password-done after changed password && add "change password" to nav bar

--- Scheduler/accounts/urls.py
+++ Scheduler/accounts/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path, include, reverse_lazy
 from . import views
 from django.contrib.auth import views as auth_views
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('change-password/<int:pk>',
         auth_views.PasswordChangeView.as_view(
             template_name='accounts/change_password.html',
-            success_url='accounts/change-password-done'
+            success_url=reverse_lazy('accounts:change-password-done')
         ),
         name='change-password'
     ),
